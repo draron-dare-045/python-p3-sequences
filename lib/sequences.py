a3f686df9f1aadcc96b840afb611c46c8519f6f5
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-05-12 06:47:10 +0300

    4 passing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    fibonacci = []
+
+    
+    for i in range(length):
+        if i == 0:
+            fibonacci.append(0)  
+        elif i == 1:
+            fibonacci.append(1)  
+        else:
+            
+            next_number = fibonacci[i - 1] + fibonacci[i - 2]
+            fibonacci.append(next_number)
+    
+    
+    print(fibonacci)
